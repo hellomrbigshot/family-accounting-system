56c72c6be16ffd83702fdb6de39720750d69b0f4
cye2 <cye2@leqee.com>
2025-06-19 19:04:47 +0800

    fix: update manifest and service worker to remove unused icon sizes and increment cache version

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'family-accounting-v1';
+const CACHE_NAME = 'family-accounting-v2';
 const OFFLINE_URL = '/offline.html';
 
 // 需要预缓存的资源
@@ -8,12 +8,8 @@ const urlsToCache = [
   '/offline.html',
   '/manifest.json',
   '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
   '/icons/icon-152x152.png',
   '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'
 ];
 
